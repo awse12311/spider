7abab4d98acb6dbccac43fed66290e540f10e95a
awse12311 <awse12311@gmail.com>
2024-05-02 11:04:30 +0800

    Update ys.js

--- ys.js
+++ ys.js
@@ -1,28 +1,37 @@
+// Get the Data Mapper output data
+var inputData = Watch.GetVariable("DataMapperOutput");
 
-// 创建一个 XMLHttpRequest 对象
-var xhr = new XMLHttpRequest();
+// If the data exists
+if (inputData) {
+    // Create an XML document object
+    var xmlDoc = new ActiveXObject("Msxml2.DOMDocument.6.0");
+    
+    // Create the root element
+    var root = xmlDoc.createElement("Root");
+    xmlDoc.appendChild(root);
+    
+    // Convert the Data Mapper output data to XML format
+    for (var i = 0; i < inputData.RowCount; i++) {
+        var row = inputData.GetRow(i);
+        var item = xmlDoc.createElement("Item");
+        for (var j = 0; j < inputData.ColumnCount; j++) {
+            var columnName = inputData.GetColumnName(j);
+            var columnValue = row[columnName];
+            var element = xmlDoc.createElement(columnName);
+            element.text = columnValue;
+            item.appendChild(element);
+        }
+        root.appendChild(item);
+    }
+    
+    // Save the XML file
+    var outputPath = "C:\\path\\to\\output\\file.xml"; // Replace with the path to save the XML file
+    xmlDoc.save(outputPath);
+    
+    // Log success message
+    Watch.Log("XML file generated successfully: " + outputPath);
+} else {
+    // If the data does not exist, log error message
+    Watch.Log("Data Mapper output data not found.");
+}
 
-// 设置请求方法和 URL，替换 'localfile.js' 为您的本地 JavaScript 文件的路径
-xhr.open('GET', 'file:///path/to/your/localfile.js', true);
-
-// 设置请求完成后的回调函数
-xhr.onload = function() {
-  // 当请求完成时
-  if (xhr.status >= 200 && xhr.status < 300) {
-    // 如果请求成功
-    var responseData = xhr.responseText; // 获取响应数据
-    // 在这里处理响应数据，例如将其传递给工作流程的下一个步骤
-    Watch.SetVariable("ExternalData", responseData); // 将响应数据保存到工作流程变量中
-  } else {
-    // 如果请求失败，可以进行相应的处理
-    console.error('请求失败: ' + xhr.status);
-  }
-};
-
-// 设置请求出错时的回调函数
-xhr.onerror = function() {
-  console.error('请求出错');
-};
-
-// 发送请求
-xhr.send();
