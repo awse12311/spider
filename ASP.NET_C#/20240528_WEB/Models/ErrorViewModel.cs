c20eaa4369d29a795d565c235d9f5f159024ea41
awse12311 <awse12311@gmail.com>
2024-05-28 13:22:23 +0800

    Create Asp.Net

---
+++ ASP.NET_C#/20240528_WEB/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _20240528_WEB.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
